9fa5ac13fcbe3d5e14638fd1f25110ec025e1985
장한울 <swingjhw@naver.com>
2024-09-12 17:14:30 +0900

    feat: tanstack:react-query 패키지 추가 및 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,27 @@ import normalize from 'emotion-normalize';
 import { Global, css } from '@emotion/react';
 import { PageLayout } from '_tosslib/components/PageLayout';
 import { KeypadPage } from 'pages/KeypadPage';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export default function App() {
+  const queryClient = new QueryClient();
   return (
     <>
-      <Global
-        styles={css`
-          ${normalize}
-          h1, h2, h3, h4, h5, h6 {
-            font-size: 1em;
-            font-weight: normal;
-            margin: 0; /* or ‘0 0 1em’ if you’re so inclined */
-          }
-        `}
-      />
-      <PageLayout>
-        <KeypadPage />
-      </PageLayout>
+      <QueryClientProvider client={queryClient}>
+        <Global
+          styles={css`
+            ${normalize}
+            h1, h2, h3, h4, h5, h6 {
+              font-size: 1em;
+              font-weight: normal;
+              margin: 0; /* or ‘0 0 1em’ if you’re so inclined */
+            }
+          `}
+        />
+        <PageLayout>
+          <KeypadPage />
+        </PageLayout>
+      </QueryClientProvider>
     </>
   );
 }
